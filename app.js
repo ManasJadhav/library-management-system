e3d5dad1c2d6317206cccc637feb8928cf090bd0
manasjadhav <manasjjadhav@gmail.com>
2023-04-04 15:31:25 +0530

    done

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"));
   });
 }
 
